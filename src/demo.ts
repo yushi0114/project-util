5c8b2d5f2a29edfa3c6f45ae80ed1ce36b5493bf
wangbinyu <binyu1231@gmail.com>
2022-09-15 17:17:18 +0800

    feat: v2

---
+++ src/demo.ts
@@ -0,0 +1,3 @@
+import { copy } from 'fs-extra'
+
+copy('../melib/melib-req-service', './dist/abc')
--- src/util.ts
+++ src/util.ts
@@ -1,32 +1,53 @@
 import { createReadStream, createWriteStream, existsSync } from 'fs'
-import { readdir, rmdir, stat, unlink } from 'fs/promises'
+import { mkdir, readdir, rmdir, stat, unlink } from 'fs/promises'
 import path from 'path'
 import { zip } from 'compressing'
 
 export function deldir(url: string) {
-  if (existsSync(url)) {
-    return readdir(url)
-      .then((items) => {
-        const itemPromises: Promise<any>[] = items.map((item) => {
-          const itemPath = path.join(url, item)
-          return stat(itemPath)
-            .then<any>((st) => {
-              return st.isDirectory()
-                ? deldir(itemPath)
-                : unlink(itemPath)
-            })
-        })
+  if (!existsSync(url))
+    return Promise.resolve('url not exist')
 
-        return Promise.all(itemPromises)
+  return readdir(url)
+    .then((items) => {
+      const itemPromises: Promise<any>[] = items.map((item) => {
+        const itemPath = path.join(url, item)
+        return stat(itemPath)
+          .then<any>((st) => {
+            return st.isDirectory()
+              ? deldir(itemPath)
+              : unlink(itemPath)
+          })
       })
-      .then(() => rmdir(url))
-  }
 
-  return Promise.resolve('url not exist')
+      return Promise.all(itemPromises)
+    })
+    .then(() => rmdir(url))
 }
 
-export function zipDir(path: string) {
-  return zip.compressDir(path, `${path}.zip`)
+export async function copyDir(from: string, to: string) {
+  if (!existsSync(from))
+    return Promise.resolve('url not exist')
+
+  if (existsSync(to))
+    await deldir(to)
+
+  await mkdir(to)
+
+  const items = await readdir(from)
+  items.forEach(async (item) => {
+    const fromUrl = path.join(from, item)
+    const toUrl = path.join(to, item)
+    const st = await stat(fromUrl)
+    if (st.isDirectory())
+      await moveFile(fromUrl, toUrl)
+    else
+      await copyDir(fromUrl, toUrl)
+  })
+}
+
+export function zipDir(path: string, to?: string) {
+  to = to || `${path}.zip`
+  return zip.compressDir(path, to)
 }
 
 export function moveFile(from: string, to: string) {
@@ -52,3 +73,8 @@ export function genDateString() {
   const dbl = toDouble
   return `${dbl(d.getMonth() + 1)}${dbl(d.getDate())}-${dbl(d.getHours())}${dbl(d.getMinutes())}`
 }
+
+export function identity(o: any) {
+  return !!o
+}
+
